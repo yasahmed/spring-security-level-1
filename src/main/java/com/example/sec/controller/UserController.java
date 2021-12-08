24779911537b6356aca63c4961d0ea586e98ef8d
Ahmed Ahmed <jnah.ahmed@gmail.com>
2021-12-08 22:20:15 +0100

    first commit

---
+++ src/main/java/com/example/sec/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.sec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value="/v1")
+public class UserController {
+
+
+    @GetMapping("/public/users")
+    public String publicUsers() {
+        return "public users";
+    }
+
+    @GetMapping("/private/users")
+    public String privateUsers() {
+        return "private users";
+    }
+}
